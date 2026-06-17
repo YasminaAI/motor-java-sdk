d0cd21432aec0ad5e803e65de77f51297d9457e7
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-06-17 11:42:52 +0000

    Release 0.0.51

--- src/main/java/com/yasminaai/api/core/ClientOptions.java
+++ src/main/java/com/yasminaai/api/core/ClientOptions.java
@@ -38,10 +38,10 @@ public final class ClientOptions {
         this.headers.putAll(headers);
         this.headers.putAll(new HashMap<String, String>() {
             {
-                put("User-Agent", "ai.yasmina:motor-java/0.0.50");
+                put("User-Agent", "ai.yasmina:motor-java/0.0.51");
                 put("X-Fern-Language", "JAVA");
                 put("X-Fern-SDK-Name", "com.yasminaai.fern:api-sdk");
-                put("X-Fern-SDK-Version", "0.0.50");
+                put("X-Fern-SDK-Version", "0.0.51");
             }
         });
         this.headerSuppliers = headerSuppliers;
